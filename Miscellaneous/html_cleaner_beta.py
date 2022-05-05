49d803e34dcf5f4777777995a1fcc86dd41be166
Yet-Again-He <jmortonor@gmail.com>
2022-05-04 19:43:17 -0700

    Reintroduced original nouns folder

--- Miscellaneous/html_cleaner_beta.py
+++ Miscellaneous/html_cleaner_beta.py
@@ -13,7 +13,10 @@ from tkinter.filedialog import askdirectory
 TOTAL = 0
 PARTIAL = 0
 
-def re_sub(test_string, current_path, working_directory):
+
+# FUNCTIONS
+
+def re_sub(test_string, current_path, working_directory, filename):
     div = re.sub("(<div[^>]+>)", "", test_string)
     span = re.sub("(<span[^>]+>)", "", div)
     pea = re.sub("(<p [^>]+>)", "<p>", span)
@@ -32,17 +35,76 @@ def re_sub(test_string, current_path, working_directory):
     # Adding a stylesheet link
     style_link = os.path.relpath(working_directory, current_path)
     style_link = style_link.replace("\\", "/")
-    css_link = f'<head>\n\t<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>\n\t<link rel="stylesheet" href="{style_link}/liberation_format.css">' # Must use / for Mac/Linux and website(?)
+    css_link = f'<head>\n\t<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>\n\t<link rel="stylesheet" href="{style_link}/liberation_format.css">'
     new_string = new_string.replace('<head>', css_link)
-    print(style_link)
+    # Remove unneeded newlines
+    new_string = new_string.replace("\n\n\n", "")
+    new_string = add_gender(new_string, current_path)
 
     return new_string
+
+
+def add_gender(old_string: str, current_path):
+    """Adds a hyperlink to the gender specification if there is no gender"""
+
+    working_directory = "../Grammar/Noun Grammar"
+    # Making directory path for noun gender and type hyperlinks
+    style_link = os.path.relpath(working_directory, current_path)
+    style_link = style_link.replace("\\", "/")
+    gen_link = f'<a href="{style_link}/Gender.htm">'
+
+    # Making Sure all keywords are capitalized correctly
+    old_string = old_string.replace("neuter", "Neuter", 1)
+    old_string = old_string.replace("common", "Common", 1)
+    old_string = old_string.replace("masculine", "Masculine", 1)
+    old_string = old_string.replace("feminine", "Feminine", 1)
+    old_string = old_string.replace("Gendered Feminine", "Gendered-Feminine", 1)
+    old_string = old_string.replace("Gendered Masculine", "Gendered-Masculine", 1)
+    old_string = old_string.replace("Gendered Common", "Gendered-Common", 1)
+
+    new_string = old_string
+
+    # Search for keywords, replace them with keyword + hyperlink if hyperlink is missing.
+    if "Neuter" in old_string:
+        if "Neuter</a>" not in old_string:
+            new_string = old_string.replace("Neuter", gen_link + "Neuter" + "</a>", 1)
+    elif "Feminine" in old_string:
+        if "Feminine</a>" not in old_string:
+            new_string = old_string.replace("Feminine", gen_link + "Feminine" + "</a>", 1)
+    elif "Masculine" in old_string:
+        if "Masculine</a>" not in old_string:
+            new_string = old_string.replace("Masculine", gen_link + "Masculine" + "</a>", 1)
+    elif "Common" in old_string:
+        if "Common</a>" not in old_string:
+            new_string = old_string.replace("Common", gen_link + "Common" + "</a>", 1)
+    elif "Gendered-Masculine" in old_string:
+        if "Gendered-Masculine</a>" not in old_string:
+            new_string = old_string.replace("Gendered-Masculine", gen_link + "Gendered-Masculine" + "</a>", 1)
+    elif "Gendered-Feminine" in old_string:
+        if "Gendered-Feminine</a>" not in old_string:
+            new_string = old_string.replace("Gendered-Feminine", gen_link + "Gendered-Feminine" + "</a>", 1)
+    elif "Gendered-Common" in old_string:
+        if "Gendered-Common</a>" not in old_string:
+            new_string = old_string.replace("Gendered-Common", gen_link + "Gendered-Common" + "</a>", 1)
+    print("Successful fix for Gender hyperlink!")
+
+    return new_string
+
+
+def add_title(old_string: str, filename):
+    """If there is no title in the file."""
+    if "<title>" not in old_string:
+        index_point = old_string.index("css\">")
+        new_string = old_string[:index_point] + f"\n<title>Neo-Adûnaic : {filename}</title>" + old_string[index_point:]
+    return new_string
+
 
 def add_bullet(old_string):
     pre_ula = "<ul>"
     post_ula = "</ul>"
     # Adding bullets
     ula = re.sub("(·[^<]+)", "<ul>", old_string)  # Use loop with index starting here to locate where to place </ul>
+
 
 # Acquire directory path
 directory = askdirectory()
@@ -53,7 +115,7 @@ working_directory = os.getcwd()
 # Calculate total number of files
 for root, dirs, files in os.walk(directory):
     for file in files:
-        if file[-4:] == ".htm":  # MAKE SURE TO TEST THIS!
+        if file[-4:] == ".htm":
             TOTAL += 1
 
 # Walk and clean chosen directory
@@ -72,7 +134,7 @@ for root, dirs, files in os.walk(directory):
             os.remove(file_path)
 
             # Clean File
-            new_string = re_sub(old_string, root2, working_directory)
+            new_string = re_sub(old_string, root2, working_directory, filename2)
 
             # Input edited information back into file
             fixed_file = open(root2 + "/" + filename, "w", encoding="utf-8")
